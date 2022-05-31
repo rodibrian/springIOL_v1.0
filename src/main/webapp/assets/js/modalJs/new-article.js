cff07c6161bd5d9c8252ccff07b6ba566b9b24fb
rodi brian <rodibrian.expert@gmail.com>
2022-05-31 15:19:53 +0300

    rajout des JS

--- src/main/webapp/assets/js/modalJs/new-article.js
+++ src/main/webapp/assets/js/modalJs/new-article.js
@@ -1,36 +1,68 @@
 console.log("js ici");
 $(function () {
+    // tout les champ non editable par defaut
+
+    $('#table-unite input').attr('disabled', '')
+
     // ajout du nouveau unite
     $("#btn-new-unite").click(
         function () {
-            console.log("ici");
             let table = $("#table-unite tbody");
             let length = $("#table-unite tbody tr").length;
             length++;
 
             // cacher le enregistrement
             $("#table-unite tbody tr th a.btn-success").hide();
+            $('#table-unite input').attr('disabled', '')
 
             // cacher edition et suppression du dernier
-            $("#table-unite tbody tr th #edit_"+length).hide();
-            $("#table-unite tbody tr th #del_"+length).hide();
-
+            $("#table-unite tbody tr th #edit_" + length).hide();
+            $("#table-unite tbody tr th #del_" + length).hide();
             let tr = `<tr>
-                            <th><input type="text" id="code_`+length+`" class="form-control input-sm" value="000"></th>
-                            <th><input type="text" id="niveau_`+length+`" class="form-control input-sm" value="0"></th>
-                            <th><input type="text" id="designation_`+length+`" class="form-control input-sm" value="designation"></th>
-                            <th><input type="text" id="quantite_`+length+`" class="form-control input-sm" value="0"></th>
-                            <th><input type="text" id="poids_`+length+`" class="form-control input-sm" value="0Kg"></th>
+                            <th><input type="text" id="code_` + length + `" class="form-control input-sm" value="000"></th>
+                            <th><input type="text" id="niveau_` + length + `" class="form-control input-sm" value="0"></th>
+                            <th><input type="text" id="designation_` + length + `" class="form-control input-sm" value="designation"></th>
+                            <th><input type="text" id="quantite_` + length + `" class="form-control input-sm" value="0"></th>
+                            <th><input type="text" id="poids_` + length + `" class="form-control input-sm" value="0Kg"></th>
                             <th class="d-inline-flex">
-                                <a class="btn btn-primary btn-sm" id="edit_`+length+`"><i class="uil-pen"></i></a>&nbsp;
-                                <a class="btn btn-danger btn-sm" id="del_`+length+`"><i class="uil-trash-alt"></i></a>&nbsp;
-                                <a class="btn btn-success btn-sm" id=add_`+length+`"><i class="uil-check-square"></i></a>
+                                <a class="btn btn-primary btn-sm btn-edit-unite" id="edit_` + length + `"><i class="uil-pen"></i></a>&nbsp;
+                                <a class="btn btn-danger btn-sm btn-del-unite" id="del_` + length + `"><i class="uil-trash-alt"></i></a>&nbsp;
+                                <a class="btn btn-success btn-sm btn-add-unite" id="add_` + length + `"><i class="uil-check-square"></i></a>
                             </th>
                         </tr>`;
             table.append(tr);
         }
     )
 
-    //
+    // edition ou enregistrement enregistrement
+
+    $('#table-unite').on('click', '.btn-add-unite', (function () {
+        $(this).closest('tr').find('input').attr('disabled', '');
+        $(this).hide();
+
+        console.log($(this).attr('id'))
+    }))
+
+    //suppression unite
+
+    $('#table-unite').on('click', '.btn-del-unite', (function () {
+        $(this).closest('tr').remove();
+
+        console.log($(this).attr('id'))
+    }))
+
+    // edition d'une unite
+
+    $('#table-unite').on('click', '.btn-edit-unite', (function () {
+        $(this).closest('tr').find('input').removeAttr('disabled')
+        $(this).closest('tr').find('.btn-add-unite').show()
+
+        console.log($(this).attr('id'))
+
+    }))
+
+    // click tr
+
+    $("#tmp").css('background-color','red');
 
 });
--- src/main/webapp/assets/js/modalJs/new-categorie.js
+++ src/main/webapp/assets/js/modalJs/new-categorie.js
@@ -1,30 +1,23 @@
 $(function(){
     $("#saveCategorieBtn").click(function (){
-        event.preventDefault();
-         let url = "http://localhost:8080/api/v1/categories";
-         let jsonData = {
-             libelle : $("#nomCategorie").val()
-         };
-        $.ajax({
-            type: 'POST',
-            url: 'http://localhost:8080/api/v1/categories',
-            contentType: 'application/json',
-            data: JSON.stringify(jsonData) ,
-            succes: function (data){
-               console.log(" SUCCES ")
-            },
-            error: function () {
-                console.log(' ERROR ')
-            },complete : function (data) {
-
-                let categorie = `
-                            <tr id="`+data.id+`">
-                             <td> `+data.libelle+`</td>
-                            </tr>
-                `;
-             $("#categorieTabList").append(categorie);
-
-            }
+         let url = "http://localhost:8080/ijeery-1.0-SNAPSHOT/api/v1/categories";
+         let data = $("#nomCategorie").val();
+         let jsonData = {'libelle':data};
+        $.get(url,function (data,status) {
+            console.log(data);
         });
+        // $.ajax({
+        //     url: url,
+        //     type: "POST",
+        //     data: JSON.stringify(jsonData),
+        //     contentType: "app/json",
+        //     success: function (data) {
+        //         // let output = data.map(i => "<tr><td>" + i.bookId + "</td><td>" + i.BookName + "</td><td>" + i.BookPrice + "</td></tr>");
+        //         // $("#output").html(output);
+        //         // $("#tbstock").show();
+        //     }
+        // });
     });
+
+
 });
