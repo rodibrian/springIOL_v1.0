91b30f93c5f9fa8c6e4ca386f483fa969ed1356a
rodi brian <rodibrian.expert@gmail.com>
2022-06-01 11:35:52 +0300

    menu nav update

--- src/main/webapp/assets/js/modalJs/new-categorie.js
+++ src/main/webapp/assets/js/modalJs/new-categorie.js
@@ -1,24 +1,6 @@
 $(function(){
 
-    $("#saveCategorieBtn").click(function (){
-         let url = "http://localhost:8080/ijeery-1.0-SNAPSHOT/api/v1/categories";
-         let data = $("#nomCategorie").val();
-         let jsonData = {'libelle':data};
-        $.get(url,function (data,status) {
-            console.log(data);
-        });
-        // $.ajax({
-        //     url: url,
-        //     type: "POST",
-        //     data: JSON.stringify(jsonData),
-        //     contentType: "app/json",
-        //     success: function (data) {
-        //         // let output = data.map(i => "<tr><td>" + i.bookId + "</td><td>" + i.BookName + "</td><td>" + i.BookPrice + "</td></tr>");
-        //         // $("#output").html(output);
-        //         // $("#tbstock").show();
-        //     }
-        // });
-    });
+
 
     let isUpdateOperation = false;
     let editBtnId = 1;
