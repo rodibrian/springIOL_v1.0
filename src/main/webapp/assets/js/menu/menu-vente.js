dcd8812e9b30b2b0c788ff54e0630d9c909ecad2
armand judicael <armandjudicaelratombotiana@gmail.com>
2022-08-04 19:39:01 +0300

    merge

--- src/main/webapp/assets/js/menu/menu-vente.js
+++ src/main/webapp/assets/js/menu/menu-vente.js
@@ -64,7 +64,6 @@ $(function () {
 
 
     $(namespace + '#table-liste-client tbody tr').on('dblclick', function () {
-
         get_select_affect_to_input(namespace + '#name-client', $(this).attr('id'), $(this).children().eq(0).text());
         $(namespace + '#modal-liste-client').modal('hide');
 
@@ -75,10 +74,10 @@ $(function () {
     -------------------------------------------------------------------------------*/
 
     function updatePrixUnitaire(article_id, unite_id, filialeId) {
-
+       let url = "http://localhost:8080/api/v1/articles/" + article_id + "/unites/" + unite_id + "/filiales/" + filialeId + "/prices";
         $.ajax({
             type: "get",
-            url: "http://localhost:8080/api/v1/articles/" + article_id + "/unites/" + unite_id + "/filiales/" + filialeId + "/prices",
+            url: url,
             contentType: "application/json",
             success: function (data) {
                 $(namespace + "#input-prix-unitaire").val(data);
@@ -87,16 +86,14 @@ $(function () {
     }
 
     $(document).on('dblclick', namespace + '#table-liste-article tbody tr', function () {
-
         let article_id = $(this).attr("id");
-        let unite_id = $(this).children().eq(2).attr("id");
+        let unite_id = $(this).children().eq(2).attr("value-id");
         let filialeId = $(namespace + '#filiale-id').attr("value-id");
         get_select_affect_to_input(namespace + '#designation-article',article_id,$(this).children().eq(1).text());
         $(namespace + '#modal-liste-article').modal('hide');
         set_select_option_value([unite_id, $(this).children().eq(2).text()], namespace + "#input-unite-article");
         updatePrixUnitaire(article_id, unite_id, filialeId);
         get_select_affect_to_input(namespace + "#input-prix-unitaire", "", $(this).children().eq(5).text())
-
         // après selection article, select * unite de l'article
         // ainsi que son prix
 
@@ -105,9 +102,7 @@ $(function () {
     /*------------------------------------------------------------------------------
                                             PRIX- SPECIAL ARTICLE
      -------------------------------------------------------------------------------*/
-
-    $(namespace + '#modal-prix-special .btn-enregistrer-modal').on('click', function () {
-
+    $(namespace + '#modal-prix-special .btn-enregistrer-modal').on('click', function (){
         $isRemise = $(namespace + '#check-prix-special-remise').is(':checked');
         $current_price = $(namespace + '#input-prix-unitaire').val();
         $special_value = $(namespace + '#input-prix-special').val();
@@ -115,11 +110,9 @@ $(function () {
         get_select_affect_to_input(namespace + '#input-prix-unitaire', null, $special_price_final);
         $(namespace + '#modal-prix-special').modal('hide')
     })
-
     /*------------------------------------------------------------------------------
                                             AJOUT DUN ARTICLE
      -------------------------------------------------------------------------------*/
-
     $('.btn-ajouter-article-vente').on('click', function (){
 
         $articleId = $(namespace + '#designation-article').attr('value-id');
@@ -133,7 +126,6 @@ $(function () {
         $userId = $(namespace + '#user-id').attr("value-id");
         $reference = $(namespace + '#input-reference-facture').val();
         $montant = $quantite * $prix_unitaire;
-
         $article_vente = [$designation, $unite, $quantite, $prix_unitaire, $montant];
 
         let infoArticleMagasin = {};
@@ -145,37 +137,27 @@ $(function () {
         infoArticleMagasin.quantiteAjout = $quantite;
         infoArticleMagasin.date = new Date();
         infoArticleMagasin.reference = $reference;
-
         $vente = {};
         $vente.infoArticleMagasin = infoArticleMagasin;
         $vente.client = {id:$clientId};
         $vente.montantVente = $montant;
-
         venteTab.push($vente);
+
         push_to_table_list(namespace + '#table-liste-article-vente', $articleId, $article_vente);
-
         // vider form vente
-
         $('.form-vente input').each(function () {
             if ($(this).attr('id') != 'name-client') $(this).attr('value', '');
             if ($(this).attr('type') === 'number') $(this).val(0);
         });
-
         // vide option
-
         $(namespace + "#input-unite-article option").remove();
-
         //impression_vente()
     })
-
     /*------------------------------------------------------------------------------
                                             SUPPRESSION D'UN ARTICLE
      -------------------------------------------------------------------------------*/
-
     $(namespace + '#table-liste-article-vente tbody tr').on('dblclick', function () {
-
         $(this).remove();
-
     })
 
     /*
@@ -247,35 +229,27 @@ $(function () {
 
     function generer_ticket() {
         let space = namespace + '#impression-ticket-caisse ';
-
         /*
         information ticket
          */
-
         $client = $(namespace + "#name-client").val();
         $magasin = $(namespace + "#select-magasin option:selected").text();
         $user = $('.account-user-name').text();
-
         /*
         add information
          */
-
         $(space + '.label-nom-client').text($client);
         $(space + '.label-magasin').text($magasin);
         $(space + '.label-utilisateur').text($user);
-
         $somme = 0;
-
         $(namespace + '#table-liste-article-vente tbody tr').each(function (index,tr) {
             $array = [$(tr).children().eq(0).text(),$(tr).children().eq(1).text(),$(tr).children().eq(2).text(),$(tr).children().eq(3).text(),$(tr).children().eq(4).text()]
             push_to_table_list(space + '#table-liste-ventes','',$array)
             $somme += parseFloat($(tr).children().eq(4).text());
         })
-
         $(space + '.label-subtotal-vente').text($somme + 'Ar');
         $(space + '.label-total-vente').text($somme + 'Ar');
         $(space + '.label-somme-en-lettre').text(NumberToLetter($somme) + ' ariary');
-
         // print
         $(space).printThis();
     }
